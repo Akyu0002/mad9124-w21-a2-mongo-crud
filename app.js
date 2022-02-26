23a096a6e8a3c15254aae2592737b75cb6ea9e0a
Akyu0002 <akyu0002@algonquinlive.com>
2022-02-26 16:42:46 -0500

    Install Morgan

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ mongoose
     process.exit(1);
   });
 
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/api/courses", require("./routes/courses"));
